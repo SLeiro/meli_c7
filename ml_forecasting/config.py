90db46009a8819927df0316eda4dd604425203e3
datalab <datalab>
2019-10-15 09:21:12 -0400

    Add config for full query

--- ml_forecasting/config.py
+++ ml_forecasting/config.py
@@ -9,37 +9,60 @@ DEFAULT_DTYPES = {
     'TIM_TIME_WINNING_DATE': np.int,
     'BID_QUANTITY_OK': np.int,
     'SHP_SHIPMENT_ID': np.object,
+    'CUS_CUST_ID_SEL': np.int,
+    'CUS_CUST_ID_BUY': np.int,
+    'CAT_CATEG_ID': np.int,
+    'ITE_CATALOG_PRODUCT_ID_STR': np.object,
+    'DOM_DOMAIN_ID': np.object,
+    'BID_BASE_CURRENT_PRICE': np.float,
+    'BID_CURRENT_PRICE': np.float,
+    'ITE_BASE_CURRENT_PRICE': np.float,
+    'ITE_SITE_CURRENT_PRICE': np.float,
+    'SHP_SPEED': np.object,
+    'SHP_SPEED_OFFSET': np.object,
+    'SHP_PICKING_TYPE_ID': np.object,
+    'SHP_REAL_COST': np.object,
+    'SHP_REAL_COST_USD': np.object,
+    'SHP_SEND_COST': np.object,
+    'SHP_RULE_COST': np.object,
+    'SHP_CARRIER_COST_CALCULATED': np.object,
+    'order_item_title': np.object,
+    'ITE_ATT_ATTRIBUTE_ID': np.object,
+    'ite_att_value_name': np.object,
 
+    'sender_shp_add_city_id': np.object,
+    'sender_shp_add_state_id': np.object,
+    'sender_shp_add_zip_code': np.object,
     'receiver_shp_add_city_id': np.object,
     'receiver_shp_add_state_id': np.object,
     'receiver_shp_add_zip_code': np.object,
-    
-    'TIM_DAY_WINNING_DATE': np.object,
 }
 
 
-DEFAULT_PARSE_DATES = [
+PARSE_DATES = [
     'TIM_DAY_WINNING_DATE',
 ]
 
 
 INTEREST_COLS =  [
-    'ord_order_id',
-    'ite_item_id',
-    'tim_day_winning_date',
-    'tim_time_winning_date',
-    'bid_quantity_ok',
-    'shp_shipment_id',
+    'ORD_ORDER_ID',
+    'ITE_ITEM_ID',
+    'TIM_DAY_WINNING_DATE',
+    'TIM_TIME_WINNING_DATE',
+    'BID_QUANTITY_OK',
+    'SHP_SHIPMENT_ID',
     'receiver_shp_add_city_id',
     'receiver_shp_add_state_id',
     'receiver_shp_add_zip_code',
 ]
 
+
 # Group configs by site
 CONFIGS_BY_SITE = {
     'MLB': {
         'DTYPES': DEFAULT_DTYPES,
-        'PARSE_DATES': DEFAULT_PARSE_DATES,
+        'TRAIN_DTYPES': TRAIN_DTYPES,
+        'PARSE_DATES': PARSE_DATES,
         'INTEREST_COLS': INTEREST_COLS,
     },
 }
@@ -48,3 +71,8 @@ CONFIGS_BY_SITE = {
 def required_cols(config_dict):
     required_cols = config_dict['INTEREST_COLS'].copy()
     return required_cols
+
+
+def train_dtypes(config_dict):
+    defaults = config_dict['DTYPES']
+    return {k: defaults.get(k) for k in required_cols(config_dict)}
