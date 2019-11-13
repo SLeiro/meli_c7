1e1cffa9a329830c42475519046508005899aea1
Font <federico.font@accenture.com>
2019-11-13 16:50:40 -0300

    create forecaster/test_jupyter_notebook.ipynb

--- forecaster/01_estimators/NaiveLV.py
+++ forecaster/01_estimators/NaiveLV.py
@@ -5,7 +5,7 @@ import datetime
 from sklearn.base import BaseEstimator
 
 
-class naiveLV(BaseEstimator):
+class NaiveLV(BaseEstimator):
     '''Implements a naiveLV forecast.'''
 
     def __init__(self, lag):
@@ -21,14 +21,14 @@ class naiveLV(BaseEstimator):
         self.N = N
         self.X = X
         length = len(X)
-        self.forecast = np.zeros((length+self.N,))
+        self.forecast = np.zeros((length + self.N,))
 
         if length < self.lag:
             print('Naive LV: Not enough data')
 
             return False
 
-        for i in range(0, length+ self.N):
+        for i in range(0, length + self.N):
             if i >= self.lag:
                 # ex-post
                 if i <= length + self.lag - 1:
@@ -42,8 +42,9 @@ class naiveLV(BaseEstimator):
     def predict(self, first_date):
         return self.forecast[first_date:self.N]
 
+
 if __name__ == '__main__':
-    arr = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
+    arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
     nv = naiveLV(5)
     fitted = nv.fit(arr, 4)
     print(nv.predict(1))
